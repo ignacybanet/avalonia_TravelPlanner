38b505129530d492abdd3542347b853809d25c47
ignacy <adsa@asda.com>
2025-04-29 15:29:09 +0200

    initial commit

---
+++ avalonia_TravelPlanner/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace avalonia_TravelPlanner;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
