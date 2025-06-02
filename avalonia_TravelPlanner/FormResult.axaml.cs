fe7ee604a4bd16638624fb2a5f8e503c50737034
ignacy <ignacybanet@cyfroweszkoly.pl>
2025-06-02 21:56:18 +0200

    create page layout, add photos

---
+++ avalonia_TravelPlanner/FormResult.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace avalonia_TravelPlanner;
+
+public partial class FormResult : Window
+{
+    public FormResult()
+    {
+        InitializeComponent();
+    }
+}
